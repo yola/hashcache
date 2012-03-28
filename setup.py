5fb1798450158a8ed162defead6e28009a1441cb
Justin Blake <justin@hentzia.com>
2012-03-28 12:46:57 -0400

    Set up a real setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-# find_packages(exclude=["*.tests", "*.tests.*"])
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hashcache",
+    version="0.0.1",
+    description="Wrapper for the django cache api to gracefully handle long or non-ascii keys",
+    license="MIT",
+    author="Yola, Inc.",
+    author_email="justin@yola.com",
+    url="http://github.com/yola/hashcache",
+    packages = find_packages(exclude=["tests.*",]),
+    keywords= "django cache library",
+    zip_safe = False,
+)
